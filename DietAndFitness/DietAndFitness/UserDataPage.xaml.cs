ae3f319b2ca5e59f83a25ed0a9a8bd96933d357f
TGeorge <todosi_george@protonmail.com>
2017-11-26 20:53:32 +0200

    Added Calculate button and label for results, added seperate page for formula help, added page titles

--- DietAndFitness/DietAndFitness/UserDataPage.xaml.cs
+++ DietAndFitness/DietAndFitness/UserDataPage.xaml.cs
@@ -16,14 +16,13 @@ namespace DietAndFitness
         {
             InitializeComponent();
         }
-    async void OnFormulaHelpClicked(object sender, EventArgs e)
+        async void OnFormulaHelpClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Choosing a formula", "This field controls which formula for calculating your needed calories will be used.\n" +
-                                                     "The Mifflin-St Jeor Formula is the simplest and most accurate formula according to the American Dietetic Association.\n" +
-                                                     "Men:\n 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (y) + 5\n" +
-                                                     "Women:\n 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (y) -161\n" +
-                                                     "The Katch-McArdle Formula is a variation of the Mifflin-St Jeor that will figure in your body fat for extra accuracy.\n" +
-                                                     "The Harris-Benedict Formula was created in 1919 but it tends to overstate caloirc needs by 5% and tends to skew results towards obese and young people", "OK");
+            await Navigation.PushAsync(new FormulaHelpPage());
+        }
+         void OnActivityHelpClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Activity Level", "Info about activity levels", "OK");
         }
     void OnFormulaKMSelected(object sender, EventArgs e)
         {
@@ -40,6 +39,14 @@ namespace DietAndFitness
                 BodyFatPercentageLabel.IsVisible = false;
             }
         }
+    void OnCalculateClicked(object sender, EventArgs e)
+    {
+
+            TotalCaloriesLabel.Text = "Place holder text for calorie results";
+    }
+
 
     }
+
+   
 }
